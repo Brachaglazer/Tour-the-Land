4d800d79dd67606a8dfaf554ce5089332e07bbd2
Devorah Teitelbaum <devorahbashateitelbaum@gmail.com>
2026-05-14 14:52:13 -0400

    added responsiveslides plugin

--- server/mongoserver.mjs
+++ server/mongoserver.mjs
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import "express-async-errors";
 import contactsRoutes from "./routes/contactsRoutes.mjs";
 import itineraryRoutes from "./routes/itineraryRoutes.mjs"
@@ -7,13 +9,23 @@ import reviewsRoutes from "./routes/reviewsRoutes.mjs";
 import tripsRoutes from "./routes/tripsRoutes.mjs";
 import tripUsersRoutes from "./routes/tripUsersRoutes.mjs"
 import usersRoutes from "./routes/usersRoutes.mjs"
-
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = 3000;
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicDir = path.join(__dirname, "..", "public");
+
 app.use(cors());
 app.use(express.json());
+
+app.use("/css", express.static(path.join(publicDir, "css")));
+app.use("/js", express.static(path.join(publicDir, "js")));
+app.use("/images", express.static(path.join(publicDir, "images")));
+app.use("/", express.static(path.join(publicDir, "views")));
 
 app.use("/contacts", contactsRoutes);
 app.use("/itinerary", itineraryRoutes);
